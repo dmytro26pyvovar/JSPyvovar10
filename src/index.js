c95531b43206112e5180894001bc213bb18cd15d
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-10-17 10:08:23 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,57 +1,85 @@
 import axios from "axios";
-import loader from "loader"
+import Notiflix from "notiflix";
+import loader from "loader";
+
 axios.defaults.headers.common["x-api-key"] = "live_hY3ifUu784lkMP59yEf6eKaoe0dFmeq7r0Pa7r64StdlTDGGQtUkc84SO0rhY90O";
 
 export function fetchBreeds() {
-    return axios.get("https://api.thecatapi.com/v1/images/search");
-}
+  return axios
+    .get("https://api.thecatapi.com/v1/breeds")
+    .then((response) => response.data)
+    .catch((error) => {
+      Notiflix.Notify.failure("Не вдалося знайти інформацію. Будь-ласка спробуйте пізніше.");
+      throw error;
+    });
+};
 
 export function fetchCatByBreed(breedId) {
-    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      Notiflix.Notify.failure("Не вдалося знайти інформацію. Будь-ласка спробуйте пізніше.");
+      throw error;
+    });
 };
 
-import { fetchBreeds, fetchCatByBreed } from './api/cat-api';
+const breedSelect = document.querySelector(".breed-select");
+const catInfo = document.querySelector(".cat-info");
+const breedName = document.querySelector(".breed-name");
+const description = document.querySelector(".description");
+const temperament = document.querySelector(".temperament");
+const error = document.querySelector(".error");
+const loaderText = document.querySelector(".loader")
 
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const breedSelect = document.querySelector('#breed-select');
-const catInfo = document.querySelector('.cat-info');
-const catImage = document.querySelector('#cat-image');
-const catBreed = document.querySelector('#cat-breed');
-const catDescription = document.querySelector('#cat-description');
-const catTemperament = document.querySelector('#cat-temperament');
+catInfo.style.display = "none";
+error.style.display = "none";
 
-async function populateBreeds() { try {
-        loader.style.display = 'block';
-        const response = await fetchBreeds();
-        const breeds = response.data;
-        breedSelect.innerHTML = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
-        loader.style.display = 'none';
-    } catch (err) {
-        error.style.display = 'block';
-    }
-};
 
-async function displayCatInfo(breedId) {
-    try {
-        loader.style.display = 'block';
-        const response = await fetchCatByBreed(breedId);
-        const cat = response.data[0];
-        catImage.src = cat.url;
-        catBreed.textContent = `Breed: ${cat.breeds[0].name}`;
-        catDescription.textContent = `Description: ${cat.breeds[0].description}`;
-        catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
-        catInfo.style.display = 'block';
-        loader.style.display = 'none';
-    } catch (err) {
-        error.style.display = 'block';
-    }
+
+function clearCatInfo() {
+  catInfo.style.display = "none";
+  breedName.textContent = "";
+  description.textContent = "";
+  temperament.textContent = "";
 }
 
-populateBreeds();
+fetchBreeds()
+  .then((breeds) => {
+    breeds.forEach((breed) => {
+      const option = document.createElement("option");
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+  })
+  .catch((error) => {
 
-breedSelect.addEventListener('change', () => {
-    const selectedBreedId = breedSelect.value;
-    displayCatInfo(selectedBreedId);
+  });
+
+breedSelect.addEventListener("change", (event) => {
+  const selectedBreedId = event.target.value;
+
+  clearCatInfo();
+
+  fetchCatByBreed(selectedBreedId)
+    .then((cats) => {
+      if (cats.length > 0) {
+        const cat = cats[0];
+        breedName.textContent = "Breed: " + cat.breeds[0].name;
+        description.textContent = "Description: " + cat.breeds[0].description;
+        temperament.textContent = "Temperament: " + cat.breeds[0].temperament;
+          catInfo.style.display = "block";
+          loaderText.style.display = "none";
+      } else {
+        Notiflix.Notify.failure("Котів не знайдено.");
+      }
+    })
+    .catch((error) => {
+    
+    });
 });
+
+
+
 
